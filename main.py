ecc277f08ce770afdc21b309f5dabb5bd1cef960
JanerRiv <nocuadavid408@gmail.com>
2026-05-14 11:12:49 -0500

    PROYECTO

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from modelos.Cliente import Cliente 
 
-    
 app = FastAPI()
 
 #APLICACION DE CLIENTES
 
-lista_clientes = []
-class Cliente(BaseModel):
-    id:int
-    nombre:str
-    edad:int
-    descripcion:str | None = None
-
-
+lista_clientes:list[Cliente] = []
 
 @app.get("/clientes")
 def get_clientes():
@@ -21,8 +13,9 @@ def get_clientes():
 
 @app.post("/clientes",)
 def crear_cliente(datos_cliente: Cliente):
+    datos_cliente.id = len(lista_clientes)
     lista_clientes.append(datos_cliente)
-    return {"mensaje": "cliente creado"}
+    return {"mensaje": "cliente creado correctamente"}
 
 @app.get("/clientes/{id}")
 def get_cliente(id: int):
@@ -30,3 +23,19 @@ def get_cliente(id: int):
         if cliente.id == id:
             return {"cliente": cliente}
     return {"mensaje": "cliente no encontrado"}    
+
+@app.put("/clientes/{id}")
+def editar_cliente(id: int, dato_clientes: Cliente):
+    for i, obj_cliente in enumerate(lista_clientes):
+        if obj_cliente.id == id:
+            lista_clientes[i] = dato_clientes
+            return {"mensaje": "cliente actualizado"}
+    return {"mensaje": "cliente no encontrado"}
+
+@app.delete("/clientes/{id}")
+def eliminar_cliente(id: int):
+    for i, obj_cliente in enumerate(lista_clientes):
+        if obj_cliente.id == id:
+            del lista_clientes[i]
+            return {"mensaje": f"Nombre: {obj_cliente.nombre}, Edad: {obj_cliente.edad}, ID: {obj_cliente.id} eliminado correctamente"}
+    return {"mensaje": "cliente no encontrado"}
