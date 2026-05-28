9ead16402f5f5935673ea9248d028aa38bf5bacd
nocuadavid408-boop <nocuadavid408@gmail.com>
2026-05-28 00:04:37 -0500

    feat: implementar modelos Factura y Transaccion con endpoints CRUD completos (GET, POST, PUT, DELETE) y método valor_total

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 
 from app.routers.clientes import router_clientes
-from app.routers.facturas import router_facturas
-from app.routers.transacciones import router_transacciones
 
 app = FastAPI()
 
@@ -13,7 +11,4 @@ def root():
 
 
 # ROUTERS
-
 app.include_router(router_clientes)
-app.include_router(router_facturas)
-app.include_router(router_transacciones)
